b57a94d5df784bc6eb4b6b60a6e62d0e3282d97b
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-21 15:52:37 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const allPopulations = [
+    ...document.querySelectorAll(".population")
+];
+const converted = allPopulations.map((population)=>{
+    return +population.textContent.replaceAll(",", "");
+});
+const total = converted.reduce((a, b)=>a + b, 0);
+const average = total / converted.length;
+const totalElement = document.querySelector(".total-population");
+const averageElement = document.querySelector(".average-population");
+totalElement.textContent = total.toLocaleString("en-US");
+averageElement.textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
